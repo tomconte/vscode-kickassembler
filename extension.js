585814aa0945d331e200c34afe6443e2bfa4afcf
Paul Hocker <paul@spocker.net>
2017-03-08 22:38:58 -0600

    add more functionality to extension;
    added java path for build
    added build and run command
    added vice path for run command

--- extension.js
+++ extension.js
@@ -5,11 +5,38 @@ function activate(context) {
 
     console.log('Congratulations, your extension "kickassembler" is now active!');
 
-    let disposable = vscode.commands.registerCommand('kickassembler.build', function () {
+    //  define the build and run command
+    let commandBuildRun = vscode.commands.registerCommand('kickassembler.build_run', function() {
+
+        if ( buildProgram() == 0 ) {
+
+            runProgram();
+        }
+    });
+
+    //  define the build command
+    let commandBuild = vscode.commands.registerCommand('kickassembler.build', function () {
+
+        buildProgram();
+
+    });
+
+    context.subscriptions.push(commandBuild);
+    context.subscriptions.push(commandBuildRun);
+}
+
+/**
+ *  Build the Program with the Compiler
+ */
+function buildProgram() {
+
+        let errorCode = 0;
 
         // Check path settings
         let configuration = vscode.workspace.getConfiguration('kickassembler');
         let kickAssPath = configuration.get('kickAssPath'); 
+        let javaPath = configuration.get("javaPath");
+
         if (kickAssPath == "") {
             vscode.window.showErrorMessage('Kick Assembler JAR path not defined! Set kickassembler.kickAssPath in User Settings.');
             return;
@@ -21,7 +48,7 @@ function activate(context) {
         outputChannel.clear();
         outputChannel.show();
 
-        let java = cp.spawn("java", [
+        let java = cp.spawn(javaPath, [
                 "-jar",
                 kickAssPath,
                 editor.document.fileName
@@ -29,6 +56,7 @@ function activate(context) {
 
         java.on('close', function(e) {
             outputChannel.appendLine('Child process exit code: ' + e);
+            errorCode = e;
             if (e != 0) {
                 vscode.window.showErrorMessage('Compilation failed with errors.');
             }
@@ -41,9 +69,31 @@ function activate(context) {
         java.stderr.on('data', function(data) {
             outputChannel.append('' + data);
         });
+
+        return errorCode;
+}
+
+/**
+ *  Run the Vice Emulator with the compiled Program
+ */
+function runProgram() {
+
+    let configuration = vscode.workspace.getConfiguration('kickassembler');
+    let vicePath = configuration.get("vicePath");
+
+    let editor = vscode.window.activeTextEditor;
+    let prg = editor.document.fileName;
+
+    //  very crude but will suffice for now
+    prg = prg.replace(".asm", ".prg");
+
+    let vice = cp.spawn(vicePath, [prg], {
+        detached : true,
+        stdio : 'ignore'
     });
 
-    context.subscriptions.push(disposable);
+    vice.unref();
+    
 }
 
 exports.activate = activate;
